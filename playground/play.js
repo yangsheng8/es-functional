e7a0ef84989604ba2250c08bda46c8d4fcf504fb
yangsheng8 <344182792@qq.com>
2018-05-03 18:00:50 +0800

    master

--- playground/play.js
+++ playground/play.js
@@ -4,4 +4,4 @@ var array = [1,2,3]
 //print the console
 forEach(array,(data) => console.log(data))
 //double the contents
-forEach(array,(data) => console.log(2 * data))
+forEach(array,(data) => console.log(3 * data))
